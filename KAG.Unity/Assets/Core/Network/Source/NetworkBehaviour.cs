19dcb8fa2a5769e0ae8de55c83dd35b9c5f95b93
Henson <frogmithenson@gmail.com>
2022-05-07 17:23:24 +0200

    Correct setup

---
+++ KAG.Unity/Assets/Core/Network/Source/NetworkBehaviour.cs
@@ -0,0 +1,14 @@
+using KAG.Shared;
+using UnityEngine;
+
+public class NetworkBehaviour : MonoBehaviour
+{
+    public Player Info { get; private set; }
+    public bool IsActive { get; set; }
+
+    public void AssignTo(Player player) => 
+        Info = player;
+
+    public void UpdateData(Player player) => 
+        Info = player;
+}
